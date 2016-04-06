8613c9acb8bcadf3924076014f19581deb1c7f20
thomasweld <thomas.weld@gmail.com>
2016-04-06 18:16:28 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+https://online.theironyard.com/library/paths/100/units/1042/assignments/1740
+
+Learning Objectives
+After completing this assignment, you should:
+
+Understand functions
+Understand how to break down a simple problem
+Performance Objectives
+After completing this assignment, you be able to effectively use
+
+JavaScript functions
+The return keyword
+JavaScript parameters
+Normal Mode
+Copy down the file excercises-1.js and write the functions it asks you to write.
+Copy down the file excercises-2.js and use the already set up function and parameters and add the statements that will preform the desired outcome.
---
+++ js/excercises-1.js
@@ -0,0 +1,99 @@
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+////    ______                _   _                ////
+////    |  ___|              | | (_)               ////
+////    | |_ _   _ _ __   ___| |_ _  ___  _ __     ////
+////    |  _| | | | '_ \ / __| __| |/ _ \| '_ \    ////
+////    | | | |_| | | | | (__| |_| | (_) | | | |   ////
+////    \_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|   ////
+////                                               ////
+////                                               ////
+////    ______               _   _                 ////
+////    | ___ \             | | (_)                ////
+////    | |_/ / __ __ _  ___| |_ _  ___ ___        ////
+////    |  __/ '__/ _` |/ __| __| |/ __/ _ \       ////
+////    | |  | | | (_| | (__| |_| | (_|  __/       ////
+////    \_|  |_|  \__,_|\___|\__|_|\___\___|       ////
+////                                               ////
+////                                               ////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+
+// Q1
+// Write a function called `sum` that takes two
+// parameters and returns the sum of those 2 numbers.
+
+// function sum ( x, y ){
+//     return Number(x) + Number(y);
+// }
+
+// OR ?
+
+function sum ( x, y ){
+    x = Number(x)
+    y = Number(y);
+    return x + y;
+}
+
+// Q2
+// Write a function named `avg` that takes 3 parameters
+// and returns the average of those 3 numbers.
+
+function avg ( x, y, z ){
+  return ( Number(x) + Number(y) + Number(z)) / 3;
+}
+
+
+// Q3
+// Write a function called `getLength` that takes one
+// parameter (a string) and returns the length
+
+function getLength (aString){
+  return aString.length;
+}
+
+
+// Q4
+// Write a function called `greaterThan` that takes
+// two parameters and returns `true` if the second
+// parameter is greater than the first. Otherwise
+// the function should return `false`.
+
+function greaterThan (param1, param2){
+  return param1 < param2;
+}
+
+
+// Q5
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+
+function greet (name){
+  return "Hello, " + name;
+}
+
+
+// Q6
+// Write a function called `madlib` that takes
+// 4 or more parameters (words). The function
+// should insert the words into a pre-defined
+// sentence. Finally the function should return
+// that sentence.
+// Note: When I say words and sentence I mean
+// strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+
+function madlib (word1, word2, word3, word4){
+  return "Iron " + word1 + " Frontend " + word2 + " Class is " + word3 + "! WhooHoo !! " + word4 + ", YEAH !!"
+}
+
+// MADLIB RESULTS 
+// madlib("Yard", "Dev", "Awesome", "JavaScript")
+// "Iron Yard Frontend Dev Class is Awesome! WhooHoo !! JavaScript, YEAH !!"
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
