3beecd1933c3d5002f6153ffcdeb8dba8c38e7da
thomasweld <thomas.weld@gmail.com>
2016-04-06 23:29:13 -0400

    turning in

--- js/excercises-2.js
+++ js/excercises-2.js
@@ -46,19 +46,42 @@ function isVowel(char){
 // Write a function translate() that will translate a text into "Rovarspraket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
-function rovarspraket(phrase){
-  phrase = phrase.toLowerCase();
-  var n = phrase.search(/[aeiuo]/);
-  switch (n){
-    case 0: phrase = phrase.
-  }
-}
+// function rovarspraket(str) {
+//   var result;
+//   if (str.search(subtext) != -1) {
+//     result = ' contains ';
+//   } else {
+//     result = ' does not contain ';
+//   }
+//   console.log(str + result + subtext);
+// }
 
+//
+// function rovarspraket(phrase){
+//   phrase = phrase.toLowerCase();
+//
+//   for (var x = 0; x < str.length; x++){
+//     var c = str.charAt(x);
+//     if ( phrase.search(/[aeiuo]/)  )
+// 
+//     alert(c);
+// }
+//
+//   var n = phrase.search(/[aeiuo]/);
+//   switch (n){
+//     case 0: phrase = phrase.
+//   }
+//
+//   phrase.replace(regexp|substr, newSubStr|function[, flags])
+//
+// }
+//
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse(){
-    //...
+function reverse(str){
+  var str = str.split('').reverse().join('');
+  return str;
 }
